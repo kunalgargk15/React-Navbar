81779c077da6d9aa0438835af16beebeca875056
Your Name <Your Email>
2023-03-29 00:10:51 +0530

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+export default function Navbar(){
+    return (<nav className="nav">
+        <Link to="/" className='site-title'>Navbar</Link>
+        <ul>
+            <CustomLink to="/home">Home</CustomLink>
+            <CustomLink to="/pricing">Pricing</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
+        </ul>
+    </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const path = window.location.pathname;
+    return(
+        <li className={path === to ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    );
+}
